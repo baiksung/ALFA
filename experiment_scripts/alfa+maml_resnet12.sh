82834db828fc773ee8e2c28ea017fc1cc6737e0b
David Baik <dsybaik@snu.ac.kr>
2020-10-25 14:59:10 +0900

    Fix multi-gpu script

--- experiment_scripts/alfa+maml_resnet12.sh
+++ experiment_scripts/alfa+maml_resnet12.sh
@@ -6,6 +6,6 @@ echo $GPU_ID
 
 cd ..
 export DATASET_DIR="datasets/"
-export CUDA_VISIBLE_DEVICES=$1, $2, $3, $4
+export CUDA_VISIBLE_DEVICES=$1,$2,$3,$4
 # Activate the relevant virtual environment:
 python train_maml_system.py --name_of_args_json_file experiment_config/alfa+maml_resnet12.json --gpu_to_use $GPU_ID
